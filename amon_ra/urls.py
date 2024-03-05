8d187642d32b057e5435b8125ea130170ad2e5de
Alexander Chaika <manti.by@gmail.com>
2024-03-05 15:32:17 +0300

    Update nginx config, linter fixes

--- amon_ra/urls.py
+++ amon_ra/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
-from django.views.generic import TemplateView
 
 from amon_ra.apps.core.views import index
 
